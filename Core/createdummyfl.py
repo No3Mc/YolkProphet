828d5b3b776936e70661bdc05892e3970be060e4
3 <41834061+No3Mc@users.noreply.github.com>
2023-01-29 20:55:49 +0000

    one-hot encoded season to make the forecast accurate

--- Core/createdummyfl.py
+++ Core/createdummyfl.py
@@ -6,5 +6,8 @@ data = pd.DataFrame(columns=['Date', 'Price', 'Temperature','Location','Season',
 # Add the current date, price, and temperature to the DataFrame
 data = data.append({'Date': '01-01-2021', 'Price': 2.5, 'Temperature': 20,'Location':'New York','Season':'Winter','Supply_Demand':'High','External_Factor':'None'}, ignore_index=True)
 
-# Save the DataFrame to a CSV file
-data.to_csv('egg_prices.csv', index=False)
+# One-hot encoding for seasons
+data = pd.get_dummies(data, columns=['Season'])
+
+# Save the DataFrame to a CSV file with dummy variables
+data.to_csv('egg_prices_with_season.csv', index=False)
--- Core/main.py
+++ Core/main.py
@@ -9,14 +9,17 @@ current_price = input("Enter the current egg price: ")
 current_date = input("Enter the current date (mm-dd-yyyy): ")
 current_temp = input("Enter the current temperature: ")
 current_location = input("Enter the location: ")
-current_season = input("Enter the current season: ")
+current_season = input("Enter the current season (summer, fall, winter, spring): ")
 current_supply_demand = input("Enter the current supply-demand ratio: ")
 current_external_factor = input("Enter any other external factors(if any): ")
 
 # Append the current data to the historical data
 data = data.append({'Price': current_price, 'Date': current_date, 'Temperature': current_temp,
-                   'Location':current_location,'Season':current_season,'Supply_Demand':current_supply_demand,
+                   'Location':current_location,'Supply_Demand':current_supply_demand,
                    'External_Factor':current_external_factor}, ignore_index=True)
+
+# Create one-hot encoded columns for the "Season" variable
+data = pd.get_dummies(data, columns=["Season"])
 
 # Fit the ARIMA model to the data
 model = ARIMA(data['Price'], order=(1,1,1))
@@ -29,7 +32,7 @@ days_to_forecast = int(input("Enter the number of days to forecast: "))
 predictions = model_fit.forecast(steps=days_to_forecast)[0]
 
 # Create a new DataFrame to store the predictions
-predictions_df = pd.DataFrame({'Date': [current_date + ' ' + i for i in range(1, days_to_forecast+1)], 'Price': predictions,'Temperature':current_temp,'Location':current_location,'Season':current_season,'Supply_Demand':current_supply_demand,'External_Factor':current_external_factor})
+predictions_df = pd.DataFrame({'Date': [current_date + ' ' + str(i) for i in range(1, days_to_forecast+1)], 'Price': predictions,'Temperature':current_temp,'Location':current_location,'Supply_Demand':current_supply_demand,'External_Factor':current_external_factor})
 
 # Append the predictions DataFrame to the original dataset
 data = data.append(predictions_df, ignore_index=True)
@@ -37,14 +40,13 @@ data = data.append(predictions_df, ignore_index=True)
 # Save the combined DataFrame to a CSV file
 data.to_csv('egg_prices.csv', index=False)
 
-# Ask user for number of days to forecast
-days_to_forecast = int(input("Enter the number of days to forecast: "))
+# Create one-hot encoded columns for the "Season" variable in the prediction dataset
+predictions_df = pd.get_dummies(predictions_df, columns=["Season"])
 
-# Make predictions for the next n days
-predictions = model_fit.forecast(steps=days_to_forecast)[0]
-
-# Create a new DataFrame to store the predictions
-predictions_df = pd.DataFrame({'Date': [current_date + ' ' + str(i) for i in range(1, days_to_forecast+1)], 'Price': predictions,'Temperature':current_temp})
+# Fill in the missing one-hot encoded columns for the "Season" variable in the prediction dataset
+missing_cols = set(data.columns) - set(predictions_df.columns)
+for c in missing_cols:
+    predictions_df[c] = 0
 
 # Append predictions to the historical data
 data = data.append(predictions_df, ignore_index=True)
@@ -53,4 +55,28 @@ data = data.append(predictions_df, ignore_index=True)
 data.to_csv('egg_prices.csv', index=False)
 
 print("Predictions have been made and added to the historical data.")
+
+# One-hot encoding for seasons
+data = pd.get_dummies(data, columns=['Season'])
+
+# Fit the ARIMA model to the data with season as a predictor variable
+model = ARIMA(data['Price'], exog=data[['Season_summer', 'Season_fall', 'Season_winter', 'Season_spring']], order=(1,1,1))
+model_fit = model.fit()
+
+# Ask user for number of days to forecast
+days_to_forecast = int(input("Enter the number of days to forecast: "))
+
+# Make predictions for the next days
+predictions = model_fit.forecast(steps=days_to_forecast, exog=data[['Season_summer', 'Season_fall', 'Season_winter', 'Season_spring']].tail(days_to_forecast))[0]
+
+# Create a new DataFrame to store the predictions
+predictions_df = pd.DataFrame({'Date': [current_date + ' ' + str(i) for i in range(1, days_to_forecast+1)], 'Price': predictions,'Temperature':current_temp,'Location':current_location,'Season':current_season,'Supply_Demand':current_supply_demand,'External_Factor':current_external_factor})
+
+# Append the predictions DataFrame to the original dataset
+data = data.append(predictions_df, ignore_index=True)
+
+# Save the combined DataFrame to a CSV file
+data.to_csv('egg_prices_with_season.csv', index=False)
+
+print("Predictions have been made and added to the historical data with season as a predictor variable.")
 
